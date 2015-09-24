f0ddaccec8ce3521ae6bcacfc5eb163de1a0a239
RAJAT GUPTA <gupta.rajat033@live.com>
2015-09-24 14:56:30 +0530

    Add a Location Setting with using Shared Preferences

--- app/src/main/java/com/example/boxerrebellion/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/boxerrebellion/sunshine/app/DetailActivity.java
@@ -29,7 +29,7 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_deatail, menu);
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
